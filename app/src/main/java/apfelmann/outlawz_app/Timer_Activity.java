980fdad1d886a5a66f2151174084407005f893f7
littlecoce <littlecoce@users.noreply.github.com>
2015-07-02 09:30:55 +0200

    TimerActivity Cleanup
    Aufräumen des Source-Codes für TimerActivity und Abändern des vererbten
    Typs zur Activity

--- app/src/main/java/apfelmann/outlawz_app/Timer_Activity.java
+++ app/src/main/java/apfelmann/outlawz_app/Timer_Activity.java
@@ -1,12 +1,12 @@
 package apfelmann.outlawz_app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Timer_Activity extends ActionBarActivity {
+public class Timer_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +14,4 @@ public class Timer_Activity extends ActionBarActivity {
         setContentView(R.layout.activity_timer_);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_timer_, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
