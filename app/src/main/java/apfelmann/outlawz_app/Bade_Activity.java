dbc52f031c3b21926beec1fc67600812d4f22df9
Apfelmann555 <apfelmanns.channel@gmx.de>
2015-07-07 08:35:41 +0200

    Datenbank Projekt erstellt.
    Datenbank

--- app/src/main/java/apfelmann/outlawz_app/Bade_Activity.java
+++ app/src/main/java/apfelmann/outlawz_app/Bade_Activity.java
@@ -1,10 +1,10 @@
 package apfelmann.outlawz_app;
 
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+        import android.app.Activity;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
 
 
 public class Bade_Activity extends Activity {
---
+++ app/src/main/java/apfelmann/outlawz_app/datenverwaltung/Timer.java
@@ -0,0 +1,7 @@
+package apfelmann.outlawz_app.datenverwaltung;
+
+/**
+ * Created by spicch on 06.07.2015.
+ */
+public class Timer {
+}
