49da8dcaae946aa47f3f51847e877de62fec3d72
Apfelmann555 <apfelmanns.channel@gmx.de>
2015-07-13 13:03:48 +0200

    Picker hinzugefügt
    Picker hinzugefügt

--- app/src/main/java/apfelmann/outlawz_app/Fischen_Activity.java
+++ app/src/main/java/apfelmann/outlawz_app/Fischen_Activity.java
@@ -1,17 +1,40 @@
 package apfelmann.outlawz_app;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.NumberPicker.OnValueChangeListener;
 
-
-public class Fischen_Activity extends Activity {
+public class Fischen_Activity extends Activity
+{
+    private TextView tv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fischen_);
+
+        setupUI();
+    }
+
+    public void setupUI()
+    {
+        tv = (TextView) findViewById(R.id.tvt);
+
+        NumberPicker np = (NumberPicker) findViewById(R.id.uhr);
+
+        np.setOnValueChangedListener(new OnValueChangeListener()
+        {
+            public void onValueChange(NumberPicker picker, int oldVal,
+                                      int newVal)
+            {
+                tv.setText(String.valueOf(newVal));
+            }
+        });
+
+        np.setMaxValue(100);
+        np.setMinValue(0);
     }
 }
